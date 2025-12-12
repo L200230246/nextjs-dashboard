7520063fc14de05f5f5495a4b66e0cc15f51b67b
thayendanega <l200230246@student.ums.ac.id>
2025-12-12 10:59:34 +0700

    perbaikan tabel

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -43,3 +43,12 @@ export type InvoicesTable = {
   status: 'paid' | 'pending';
 };
 
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
+};
